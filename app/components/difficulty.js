83c81095fa6f767560976f2a57171e4c12b5d4db
Blaine Schmeisser <blainesch@gmail.com>
2015-05-20 10:14:56 -0700

    Add local storage for preferences.

--- app/components/difficulty.js
+++ app/components/difficulty.js
@@ -1,22 +1,24 @@
 'use strict';
 
-angular.module('myApp.preferences')
+angular.module('myApp.preferences').
 
-.factory('difficulty', function() {
+factory('difficulty', ['localStorageService', function($storage) {
   var difficulty = null;
   var self = {
     difficulties: ['easy', 'medium', 'hard'],
     initialize: function() {
-      self.value(self.difficulties[0]);
+      self.value($storage.get('difficulty') || self.difficulties[0]);
     },
     value: function(value) {
       if (self.difficulties.indexOf(value) !== -1) difficulty = value;
       return difficulty;
+    },
+    save: function() {
+      $storage.set('difficulty', difficulty);
     }
   };
-  window.foo = self;
 
   self.initialize();
 
   return self;
-});
+}]);
